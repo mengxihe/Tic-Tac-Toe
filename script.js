8283108a68327837e56659639ee3c6c42313153b
Mengxi He <mengxihex@gmail.com>
2022-08-06 22:06:10 +0200

    small changes

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 console.log('Hi')
 
 const GameBoard = (() => {
+    const row = 3;
+    const col = 3;
+    const vacant = "white";
+    const sq = 100;
     const gameboard = ["X", "X", "X", "O", "O", "O", "X", "X", "X"]
     const render = (board) => {
         nHTML = '';
@@ -9,6 +13,9 @@ const GameBoard = (() => {
         return display;
     }
     output = render(gameboard)
+    const buildGrid = () => {
+        
+    }
     return {
         output,
     };
